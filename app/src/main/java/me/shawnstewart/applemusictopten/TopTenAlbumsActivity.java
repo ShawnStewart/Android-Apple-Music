c91e07da60792741bd5b3b6a9de83f2a2a0239fb
Shawn Stewart <s.stewart0220@gmail.com>
2019-01-10 16:39:33 -0500

    Initialized an app with an empty activity. Added a button to start another activity for displaying the top 10 albums.

---
+++ app/src/main/java/me/shawnstewart/applemusictopten/TopTenAlbumsActivity.java
@@ -0,0 +1,13 @@
+package me.shawnstewart.applemusictopten;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopTenAlbumsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_ten_albums);
+    }
+}
